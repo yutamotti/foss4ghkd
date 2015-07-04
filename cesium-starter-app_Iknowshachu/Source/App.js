cbe33f1fb4c0d725c40d31c2d51b7deef23b5682
yutamotti <shirai@oyorm.co.jp>
2015-07-04 11:26:05 +0900

    ktest
    ktest

--- cesium-starter-app_Iknowshachu/Source/App.js
+++ cesium-starter-app_Iknowshachu/Source/App.js
@@ -1,3 +1,5 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
-viewer.camera.viewRectangle(Cesium.Rectangle.fromDegrees(114.591, -45.837, 148.970, -5.730));
+// viewer.camera.viewRectangle(Cesium.Rectangle.fromDegrees(114.591, -45.837, 148.970, -5.730));
+
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://yutamotti.github.io/foss4ghkd/cesium-starter-app-Ikowshachu/Source/20150704KML.kml'));
